d1df1134c5f2366b38fda6de23e6644a9e38cd8d
Valyy <valyy.c17@gmail.com>
2018-10-19 17:25:11 +0300

    19.2.0

--- p1/ec2-2.js
+++ p1/ec2-2.js
@@ -1,50 +1,42 @@
-document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.10.12.3";
+document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.10.19.3";
 //-------------------------------
-function read_data()
+
+function EC2()
 {
-	var _a = document.getElementById("id_a").value;
-	var _b = document.getElementById("id_b").value;
-	var _c = document.getElementById("id_c").value;	
-	var coef = {a:_a, b:_b, c:_c};
-	return coef;
-} 
-//-------------------------------
-function compute_delta(coef)
-{
-	return coef.b * coef.b - 4 * coef.a * coef.c;	
-}
-//-------------------------------
-function solve(coef)
-{
-	var delta = compute_delta(coef);
-	var x1_re, x1_im, x2_re, x2_im;
+	this.read_data = function(){
+		this.a = document.getElementById("id_a").value;
+		this.b = document.getElementById("id_b").value;
+		this.c = document.getElementById("id_c").value;
+		
+	};
+	this.solve2 = function(){
+	var delta = this.b * this.b - 4 *this.a * this.c;
+	
 	if (delta >= 0){
-		x1_re = (-coef.b - Math.sqrt(delta)) / (2 * coef.a);
-		x1_im = 0;
-		x2_re = (-coef.b + Math.sqrt(delta)) / (2 * coef.a);
-		x2_im = 0;
+		this.x1 = {re:(-coef.b - Math.sqrt(delta)) / (2 * coef.a), im:0};
+		this.x2 = {re:(-coef.b + Math.sqrt(delta)) / (2 * coef.a), im:0};
 	}
 	else{
-		x1_re = -coef.b / (2 * coef.a);
-		x1_im = -Math.sqrt(-delta) / (2 * coef.a);
-		x2_re = -coef.b / (2 * coef.a);
-		x2_im = Math.sqrt(-delta) / (2 * coef.a);
+		this.x1 = {re:-coef.b / (2 * coef.a);x1_im = -Math.sqrt(-delta) / (2 * coef.a);
+		this.x2 = {re:-coef.b / (2 * coef.a);x2_im = Math.sqrt(-delta) / (2 * coef.a);
 	}	
-	var x1 = {re:x1_re, im:x1_im};
-	var x2 = {re:x2_re, im:x2_im};
-	var solutions = {x1:x1, x2:x2};
-	return solutions;
+
+		
+	};
+	this.print_solutions = function(){
+		document.getElementById("id_x1").innerHTML = this.x1.re + " + " + this.x1.im + "i";
+		document.getElementById("id_x2").innerHTML = this.x2.re + " + " + this.x2.im + "i";
+		
+	};
+	
 }
-//-------------------------------
-function print_solutions(solutions)
-{
-	document.getElementById("id_x1").innerHTML = solutions.x1.re + " + " + solutions.x1.im + "i";
-	document.getElementById("id_x2").innerHTML = solutions.x2.re + " + " + solutions.x2.im + "i";	
-}
-//-------------------------------
+
+
 function solve()
 {
-	var coef = read_data();
-	var solutions = solve2(coef);
-	print_solutions(solutions);
+	var ec2 = new EC2();
+	ec2.read_data();
+	ec2.solve2();
+	ec2.print_solutions();
+	
 }
