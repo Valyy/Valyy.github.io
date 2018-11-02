cb1f41ef7e6c0fcdcb9527c87e679f7f76f06042
Valyy <valyy.c17@gmail.com>
2018-11-02 18:25:25 +0200

    or 1.1

--- p1/orientare.js
+++ p1/orientare.js
@@ -4,6 +4,6 @@ window.addEventListener("deviceorientation", on_device_orientation);
 function on_device_orientation()
 {
   document.getElementById("id_z").innerHTML = e.alpha;
-  document.getElementById("id_x").innerHTML = e.gamma;
-  document.getElementById("id_y").innerHTML = e.beta;
+  document.getElementById("id_x").innerHTML = e.beta;
+  document.getElementById("id_y").innerHTML = e.gamma;
 }
