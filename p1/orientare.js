c37b1c8268c44e43ddaa2f779cef2eb6af9abc3d
Valyy <valyy.c17@gmail.com>
2018-11-02 18:36:50 +0200

    gooo1.-0

--- p1/orientare.js
+++ p1/orientare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02.0";
+document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02.2";
 window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e)
